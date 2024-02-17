530b14073bcef45711635cebc5f1e3609008a37c
chandu916 <cchandhan021@gmail.com>
2024-02-17 10:18:21 +0530

    icons theme img -> svg

--- client/src/components/LeftSidebar/LeftSidebar.jsx
+++ client/src/components/LeftSidebar/LeftSidebar.jsx
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom'
 import Globe from '../../assets/Globe.svg'
 import user from '../../assets/user.svg'
 import tags from '../../assets/tags.svg'
-import home from '../../assets/home.svg'
 import save from '../../assets/save.svg'
 const LeftSidebar = () => {
     
@@ -12,27 +11,27 @@ const LeftSidebar = () => {
     <div className='left-sidebar'>
         <nav className='side-nav'>
             <NavLink to='/' className='side-nav-links' activeClassName='active'>
-                <img src={home} alt="home" width={18} />
+            <svg xmlns="http://www.w3.org/2000/svg" className='icons' viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
                 <p style={{paddingLeft:'10px'}}>Home</p>
             </NavLink>
             <div className='side-nav-div'>
                 <div> <p> Public</p></div>
                 <NavLink to='/Questions' className='side-nav-links'>
-                    <img src={Globe}alt="Globe" width={18}/>
+                <svg aria-hidden="true" className='icons' width="18" height="18" viewBox="0 0 18 18"><path d="m4 15-3 3V4c0-1.1.9-2 2-2h12c1.09 0 2 .91 2 2v9c0 1.09-.91 2-2 2H4Zm7.75-3.97c.72-.83.98-1.86.98-2.94 0-1.65-.7-3.22-2.3-3.83a4.41 4.41 0 0 0-3.02 0 3.8 3.8 0 0 0-2.32 3.83c0 1.29.35 2.29 1.03 3a3.8 3.8 0 0 0 2.85 1.07c.62 0 1.2-.11 1.71-.34.65.44 1 .68 1.06.7.23.13.46.23.7.3l.59-1.13a5.2 5.2 0 0 1-1.28-.66Zm-1.27-.9a5.4 5.4 0 0 0-1.5-.8l-.45.9c.33.12.66.29.98.5-.2.07-.42.11-.65.11-.61 0-1.12-.23-1.52-.68-.86-1-.86-3.12 0-4.11.8-.9 2.35-.9 3.15 0 .9 1.01.86 3.03-.01 4.08Z"></path></svg>
                     <p style={{paddingLeft:'10px'}}>Questions</p>
                 </NavLink>
 
                 <NavLink to='/Tags' className='side-nav-links' activeClassName='active' >
-                    <img src={tags}alt="Globe" width={18}/>
+                <svg aria-hidden="true" className='icons'  width="18" height="18" viewBox="0 0 18 18"><path d="M9.24 1a3 3 0 0 0-2.12.88l-5.7 5.7a2 2 0 0 0-.38 2.31 3 3 0 0 1 .67-1.01l6-6A3 3 0 0 1 9.83 2H14a3 3 0 0 1 .79.1A2 2 0 0 0 13 1H9.24Z" opacity=".4"></path><path d="M9.83 3a2 2 0 0 0-1.42.59l-6 6a2 2 0 0 0 0 2.82L6.6 16.6a2 2 0 0 0 2.82 0l6-6A2 2 0 0 0 16 9.17V5a2 2 0 0 0-2-2H9.83ZM12 9a2 2 0 1 1 0-4 2 2 0 0 1 0 4Z"></path></svg>
                     <p style={{paddingLeft:'10px'}}>Tags</p>
                 </NavLink>
 
                 <NavLink to='/Saved' className='side-nav-links' activeClassName='active' >
-                    <img src={save}alt="save" width={14}/>
+                <svg aria-hidden="true" className='icons' width="18" height="18" viewBox="0 0 18 18"><path d="M3 17V3c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v14l-6-4-6 4Z"></path></svg>
                     <p style={{paddingLeft:'10px'}}>  Saved</p>
                 </NavLink>
                 <NavLink to='/Users' className='side-nav-links' activeClassName='active' >
-                    <img src={user}alt="Globe" width={18}/>
+                <svg aria-hidden="true" className='icons' width="18" height="18" viewBox="0 0 18 18"><path d="M17 14c0 .44-.45 1-1 1H9a1 1 0 0 1-1-1H2c-.54 0-1-.56-1-1 0-2.63 3-4 3-4s.23-.4 0-1c-.84-.62-1.06-.59-1-3 .06-2.42 1.37-3 2.5-3s2.44.58 2.5 3c.06 2.41-.16 2.38-1 3-.23.59 0 1 0 1s1.55.71 2.42 2.09c.78-.72 1.58-1.1 1.58-1.1s.23-.4 0-1c-.84-.61-1.06-.58-1-3 .06-2.41 1.37-3 2.5-3s2.44.59 2.5 3c.05 2.42-.16 2.39-1 3-.23.6 0 1 0 1s3 1.38 3 4Z"></path></svg>
                     <p style={{paddingLeft:'10px'}}>Users</p>
                 </NavLink>
             </div>
--- client/src/components/RightSidebar/Widget.jsx
+++ client/src/components/RightSidebar/Widget.jsx
@@ -10,22 +10,22 @@ const Widget = () => {
         <h4>The Overlow Blog</h4>
         <div className='right-sidebar-div-1'>
             <div className='right-sidebar-div-2'>
-                <img src={pen} alt="Pen" width={12} />
+            <svg xmlns="http://www.w3.org/2000/svg" fill='var(--font-clr)' height="16" width="16" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
                 <p>Letting algorithms guide our path to the next great invention</p>
             </div>
             <div className='right-sidebar-div-2'>
-                <img src={pen} alt="Pen" width={12} />
+            <svg xmlns="http://www.w3.org/2000/svg" fill='var(--font-clr)' height="16" width="16" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
                 <p>The half-life of developer skills is shrinking rapidly</p>
             </div>
         </div>
         <h4>Featured on Meta</h4>
         <div className='right-sidebar-div-1'>
             <div className='right-sidebar-div-2'>
-                <img src={message} alt="message" width={12} />
+            <svg xmlns="http://www.w3.org/2000/svg" fill='var(--font-clr)' width="25" viewBox="0 0 512 512"><path d="M160 368c26.5 0 48 21.5 48 48v16l72.5-54.4c8.3-6.2 18.4-9.6 28.8-9.6H448c8.8 0 16-7.2 16-16V64c0-8.8-7.2-16-16-16H64c-8.8 0-16 7.2-16 16V352c0 8.8 7.2 16 16 16h96zm48 124l-.2 .2-5.1 3.8-17.1 12.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3V474.7v-6.4V468v-4V416H112 64c-35.3 0-64-28.7-64-64V64C0 28.7 28.7 0 64 0H448c35.3 0 64 28.7 64 64V352c0 35.3-28.7 64-64 64H309.3L208 492z"/></svg>
                 <p>Sites can now request to enable a banner to warn about their policy on...</p>
             </div>
             <div className='right-sidebar-div-2'>
-                <img src={blacklogo} alt="stack" width={12} />
+            <svg xmlns="http://www.w3.org/2000/svg" fill='var(--font-clr)' width="18" viewBox="0 0 384 512"><path d="M290.7 311L95 269.7 86.8 309l195.7 41zm51-87L188.2 95.7l-25.5 30.8 153.5 128.3zm-31.2 39.7L129.2 179l-16.7 36.5L293.7 300zM262 32l-32 24 119.3 160.3 32-24zm20.5 328h-200v39.7h200zm39.7 80H42.7V320h-40v160h359.5V320h-40z"/></svg>
                 <p>Temporary policy: Generative AI (e.g., ChatGPT) is banned</p>
             </div>
         </div>
