4740bf52e62abcf2b7335bef4c0cb64cf6866b33
chandu916 <cchandhan021@gmail.com>
2024-02-17 22:56:30 +0530

    updates

--- client/src/pages/UserProfile/UserProfile.jsx
+++ client/src/pages/UserProfile/UserProfile.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
-import pen from '../../assets/pen.svg';
-import cake from '../../assets/cake.svg'
 import moment from "moment";
 
 import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
 import Avatar from "../../components/Avatar/Avatar";
 import EditProfileForm from "./EditProfileForm";
 import ProfileBio from "./ProfileBio";
+import LoginHistory from "./LoginHistory";
 import "./UsersProfile.css";
 
 const UserProfile = ({ slideIn, handleSlideIn }) => {
@@ -38,24 +37,49 @@ const UserProfile = ({ slideIn, handleSlideIn }) => {
               <div className="user-name">
                 <h1>{currentProfile?.name}</h1>
                 <p>
-                  <img src={cake} alt="" width={15}/> Joined{" "}
+                <svg xmlns="http://www.w3.org/2000/svg" className="icons" viewBox="0 0 448 512"><path d="M86.4 5.5L61.8 47.6C58 54.1 56 61.6 56 69.2V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L105.6 5.5C103.6 2.1 100 0 96 0s-7.6 2.1-9.6 5.5zm128 0L189.8 47.6c-3.8 6.5-5.8 14-5.8 21.6V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L233.6 5.5C231.6 2.1 228 0 224 0s-7.6 2.1-9.6 5.5zM317.8 47.6c-3.8 6.5-5.8 14-5.8 21.6V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L361.6 5.5C359.6 2.1 356 0 352 0s-7.6 2.1-9.6 5.5L317.8 47.6zM128 176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48c-35.3 0-64 28.7-64 64v71c8.3 5.2 18.1 9 28.8 9c13.5 0 27.2-6.1 38.4-13.4c5.4-3.5 9.9-7.1 13-9.7c1.5-1.3 2.7-2.4 3.5-3.1c.4-.4 .7-.6 .8-.8l.1-.1 0 0 0 0s0 0 0 0s0 0 0 0c3.1-3.2 7.4-4.9 11.9-4.8s8.6 2.1 11.6 5.4l0 0 0 0 .1 .1c.1 .1 .4 .4 .7 .7c.7 .7 1.7 1.7 3.1 3c2.8 2.6 6.8 6.1 11.8 9.5c10.2 7.1 23 13.1 36.3 13.1s26.1-6 36.3-13.1c5-3.5 9-6.9 11.8-9.5c1.4-1.3 2.4-2.3 3.1-3c.3-.3 .6-.6 .7-.7l.1-.1c3-3.5 7.4-5.4 12-5.4s9 2 12 5.4l.1 .1c.1 .1 .4 .4 .7 .7c.7 .7 1.7 1.7 3.1 3c2.8 2.6 6.8 6.1 11.8 9.5c10.2 7.1 23 13.1 36.3 13.1s26.1-6 36.3-13.1c5-3.5 9-6.9 11.8-9.5c1.4-1.3 2.4-2.3 3.1-3c.3-.3 .6-.6 .7-.7l.1-.1c2.9-3.4 7.1-5.3 11.6-5.4s8.7 1.6 11.9 4.8l0 0 0 0 0 0 .1 .1c.2 .2 .4 .4 .8 .8c.8 .7 1.9 1.8 3.5 3.1c3.1 2.6 7.5 6.2 13 9.7c11.2 7.3 24.9 13.4 38.4 13.4c10.7 0 20.5-3.9 28.8-9V288c0-35.3-28.7-64-64-64V176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48H256V176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48H128V176zM448 394.6c-8.5 3.3-18.2 5.4-28.8 5.4c-22.5 0-42.4-9.9-55.8-18.6c-4.1-2.7-7.8-5.4-10.9-7.8c-2.8 2.4-6.1 5-9.8 7.5C329.8 390 310.6 400 288 400s-41.8-10-54.6-18.9c-3.5-2.4-6.7-4.9-9.4-7.2c-2.7 2.3-5.9 4.7-9.4 7.2C201.8 390 182.6 400 160 400s-41.8-10-54.6-18.9c-3.7-2.6-7-5.2-9.8-7.5c-3.1 2.4-6.8 5.1-10.9 7.8C71.2 390.1 51.3 400 28.8 400c-10.6 0-20.3-2.2-28.8-5.4V480c0 17.7 14.3 32 32 32H416c17.7 0 32-14.3 32-32V394.6z"/></svg> Joined{" "}
                   {moment(currentProfile?.joinedOn).fromNow()}
                 </p>
               </div>
             </div>
               {currentUser?.result._id === id && (
                 <button type="button" onClick={() => setSwitch(true)} className="edit-profile-btn">
-                  <img src={pen} alt="pen" width={15} />  Edit Profile
+                  <svg xmlns="http://www.w3.org/2000/svg" className="icons" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>  Edit Profile
                 </button>
               )}
-          </div>  
-          <>
-            {Switch ? (
-              <EditProfileForm currentUser={currentUser} setSwitch={setSwitch} /> ) : 
-              (
-              <ProfileBio currentProfile={currentProfile} />
+            </div>
+
+            <div className="" style={{marginTop:'10px'}}>
+            {currentUser?.result._id === id && (
+                <button  onClick={() => setSwitch('profileBio')} className="nav-items nav-btn">
+                  Profile
+                </button>
               )}
-          </>
+            {currentUser?.result._id === id && (
+                  <button onClick={() => setSwitch('loginHistory')} className="nav-items nav-btn">
+                    Logins
+                 </button>
+              )}
+              {currentUser?.result._id === id && (
+                <button onClick={() => setSwitch('theme')} className="nav-items nav-btn">
+                   theme
+                </button>
+              )}
+            </div>
+
+              
+          <>
+            {Switch === "editProfile" && (
+            <EditProfileForm currentUser={currentUser} setSwitch={setSwitch} />
+            )}
+            {Switch === "profileBio" && (
+            <ProfileBio currentProfile={currentProfile} setSwitch={setSwitch} />
+            )}
+            
+            {Switch === "loginHistory" && (
+              <LoginHistory />
+            )}
+            </>
         </section>
       </div>
     </div>
